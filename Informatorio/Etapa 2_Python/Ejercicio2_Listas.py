3e7ff135f40b72904657bab3db6a204b670942d3
Santiago Cabrera <109568766+SantiCabrera19@users.noreply.github.com>
2023-05-01 19:40:12 -0300

    Subo Carpeta (Informatorio)
    Carpeta donde voy a subir ejercicios que hago para el INFORMATORIO | CHACO

---
+++ Informatorio/Etapa 2_Python/Ejercicio2_Listas.py
@@ -0,0 +1,12 @@
+
+#Creamos la lista con las cuidades
+ciudades = ["Manchester", "Milan", "Resistencia", "Londres"]
+
+#Mostramos la lista con las cuidades
+print(ciudades)
+
+#Agregamos una ciudad
+ciudades.append("New York");
+
+#Mostramos la lista con la nueva cuidad agregada
+print(ciudades)
