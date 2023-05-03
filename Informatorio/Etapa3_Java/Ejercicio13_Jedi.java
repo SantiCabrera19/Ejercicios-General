6b008471bba7ccab0c103db815e2677c99069be8
Santiago Cabrera <cabrerasanty321@gmail.com>
2023-05-03 20:25:35 -0300

    Subo Ejercicios JAVA conversion Dolares

--- Informatorio/Etapa3_Java/Ejercicio13_Jedi.java
+++ Informatorio/Etapa3_Java/Ejercicio13_Jedi.java
@@ -3,21 +3,22 @@ package Informatorio.Etapa3_Java;
 // importamos el Scanner
 import java.util.Scanner;
 
-public class Ejercicio12_Jedi {
+public class Ejercicio13_Jedi {
     public static void main(String[] args) {
         // Creamos el Scanner
         Scanner input = new Scanner(System.in);
 
         // Definimos las variables
-        float dolares;
-        float euros;
+        int dolares;
+        int pesos;
+
         // Solicitamos el ingreso del monto en dolares a convertir
         System.out.println("Ingrese el monto en dolares: ");
         dolares = input.nextInt();
         // La conversion..
-        euros = (float) (dolares*1.21);
+        pesos = dolares * 20;
         // Mostramos la conversion..
-        System.out.println("El valor de $" + dolares + " Dolares en Euros es de: $" + euros);
+        System.out.println("El valor de $" + dolares + " Dolares en Pesos es de: $" + pesos);
         // cerramos el input para sacar el warning...
         input.close();
     }
