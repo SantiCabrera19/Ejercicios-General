ea7e8b552ca11cc136a97fbf6ca7025c35255dda
Santiago Cabrera <cabrerasanty321@gmail.com>
2023-05-11 00:59:09 -0300

    Subo 8 y 9 de iterativas..

---
+++ Informatorio/Etapa 2_Python/Ejercicio8_iterativas.py
@@ -0,0 +1,8 @@
+"""Escribe un programa que pida al usuario una cadena de texto y luego imprima
+ el número de palabras que contiene"""
+
+cadena = input("Escribe una cadena de texto: ").split()
+palabras = len(cadena)
+print(f"la palabra tiene: {palabras} palabras")
+    
+
